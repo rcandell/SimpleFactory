4be366d491604ad1d865f73c2066c9a10681a693
rcandell <rick.candell@nist.gov>
2015-03-01 13:06:52 -0500

    add logging capability to the model.  add output storage to model

--- SimpleFactory.py
+++ SimpleFactory.py
@@ -6,6 +6,27 @@
 
 import random
 import simpy
+import logging
+from enum import Enum
+
+class EventType(Enum):
+	MACHINE_CREATED = 10
+	MACHINE_WORK = 11
+
+	THING_CREATED = 20
+	THING_ENTER_MACH = 21
+	THING_EXIT_MACH = 22
+	THING_TRAVEL = 23
+
+	PRODUCT_STORED = 50
+
+	DIAGNOSTICS = 100
+
+
+def loginfo(type, env, mach_id, thing_id, msg):
+	log_str = str(env.now) + '\t' + str(type.value) + '\t' + str(mach_id) + '\t' + str(thing_id) + '\t' + msg
+	print(log_str)
+	logging.info(log_str)
 
 class Rail(object):
 	""" A Path represents the delay between to machine stations """
@@ -15,10 +36,10 @@ class Rail(object):
 		self.path = simpy.Resource(env,1) # one path out from machine
 
 	def travel(self, thing_id):
-		yield self.env.timeout(self.t_delay)
-		print( "Thing %u traveled to next at %.3f" % (thing_id, self.env.now))
-
+		
 		# TODO: Place optical sensor reading here TCP
+
+		yield self.env.timeout(self.t_delay)
 
 
 
@@ -38,20 +59,25 @@ class Machine(object):
 		# TODO:  Wait for command from controller to start work TCP
 
 		yield self.env.timeout(self.worktime)
-		print("Machine %u completes work on Thing %u" % (self.mach_id, thing_id))
+		loginfo(EventType.MACHINE_WORK, self.env, self.mach_id, thing_id, "machining work completed")
+		#print("Machine %u completes work on Thing %u" % (self.mach_id, thing_id))
 
-def thing(env, thing_id, machines):
+def thing(env, thing_id, machines, output_store):
 	""" A thing goes to each machine and requests work to be done.
 		Work is done and thing leaves to never come back """
 	for mach in machines:
-		with mach.station.request() as request:
-			yield request
-			print('Thing %u enters machine %u at %.3f.' % (thing_id, mach.mach_id, env.now))
+		with mach.station.request() as station_request:
+			yield station_request
+			loginfo(EventType.THING_ENTER_MACH, env, mach.mach_id, thing_id, "thing enters machine")
 			yield env.process(mach.work(thing_id))
-			print('Thing %u leaves machine %u at %.3f.' % (thing_id, mach.mach_id, env.now))
-			with mach.pathout.path.request() as request:
-				yield request
+			loginfo(EventType.THING_EXIT_MACH, env, mach.mach_id, thing_id, "thing exits machine")
+			with mach.pathout.path.request() as path_request:
+				yield path_request
+				loginfo(EventType.THING_TRAVEL, env, mach.mach_id, thing_id, "thing in transit")
 				yield env.process(mach.pathout.travel(thing_id))
+	loginfo(EventType.PRODUCT_STORED, env, -1, thing_id, "thing stored as product")
+	output_store.put(1)
+	loginfo(EventType.DIAGNOSTICS, env, -1, -1, "number of products " + str(output_store.level))
 
 def setup(env, num_things, num_machines, num_stations, worktime, t_inter):
 	""" Create the factory architecture """
@@ -62,16 +88,21 @@ def setup(env, num_things, num_machines, num_stations, worktime, t_inter):
 		machines.append(m)
 		print ("Added machine %u" % mach_id)
 
+	# create the storage bin for product output
+	output_store = simpy.resources.container.Container(env, capacity=10000)
+
 	# Create more things while the simulation is running
-	for thing_id in range(num_things):
-		print('Thing %u generated' % thing_id)
+	thing_id = 0
+	while True:
+		loginfo(EventType.THING_CREATED, env, -1, thing_id, "thing created")
 		#yield env.timeout(random.randint(t_inter-2, t_inter+2))
 		yield env.timeout(t_inter)
-		env.process(thing(env, thing_id, machines))
+		env.process(thing(env, thing_id, machines, output_store))
+		thing_id += 1
 
 
 RANDOM_SEED = 42
-NUM_THINGS = 100
+NUM_THINGS = 2**32
 NUM_MACHINES = 4  # number of machines
 NUM_STATIONS = 2  # Number of stations per machine
 WORKTIME = 5      # Minutes at machine
@@ -79,6 +110,8 @@ T_INTER = 2       # Create a thing every NN minutes
 SIM_TIME = 4000     # Simulation time in minutes
 
 if __name__ == "__main__":
+
+	logging.basicConfig(filename='sim.log', level=logging.INFO)
 	
 	print('Simple Factory')
 	random.seed(RANDOM_SEED)  # This helps reproducing the results
